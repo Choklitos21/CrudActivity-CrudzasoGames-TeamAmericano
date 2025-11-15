3891e3e4a18754f686b113b2b8b6cb2703cde122
andguz8a <and.guz8a@gmail.com>
2025-11-15 00:51:45 -0500

    Listas y diccionarios completos questions.py

--- questions.py
+++ questions.py
@@ -1,1 +1,206 @@
-#mi primer commits
+preguntas = [
+        {
+            "categoria": "Ciencia",
+            "preguntas": [
+                {
+                    "pregunta": "¿Cual es el símbolo de PLATA en la tabla periódica?",
+                    "respuesta": "b",
+                    "opciones":[
+                        "a) P", "b) Ag", "c) B", "d) As"
+                    ]
+                },
+                {
+                    "pregunta": "2.	¿A qué grupo de animales pertenecen las ranas?",
+                    "respuesta": "c",
+                    "opciones":[
+                        "a) Reptiles", "b) Mamiferos", "c) Anfibios", "d) Oviparos"
+                    ]
+                },
+                {
+                    "pregunta": "Cual es el tipo de sangre mas raro?",
+                    "respuesta": "a",
+                    "opciones":[
+                        "a) AB-", "b) O+", "c) AB+", "d) A+"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cuántos huesos tiene el cuerpo humano en adultos?",
+                    "respuesta": "c",
+                    "opciones":[
+                        "a) 216", "b) 202", "c) 206", "d) 230"
+                    ]
+                }
+
+            ]
+        },
+        {
+            "categoria": "Arte",
+            "preguntas": [
+                {
+                    "pregunta": "¿En que museo esta la mona lisa?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Museo del prado", "b) Louvre", "c) MoMa", "d) Uffizi"
+                    ]
+                },
+                {
+                    "pregunta": "¿Qué técnica artística usa pigmento diluido en agua sobre yeso húmedo?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Óleo", "b) Fresco", "c) Acuarela", "d) Temple"
+                    ]
+                },
+                {
+                    "pregunta": "¿Qué edificio es un ejemplo del estilo gótico?",
+                    "respuesta": "a",
+                    "opciones": [
+                        "a) Catedral de Notre Dame", "b) Coliseo Romano", "c) Partenón", "d) Ópera de Sídney"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cual de estas esculturas es egipcia?",
+                    "respuesta": "c",
+                    "opciones": [
+                        "a) El David", "b) El Discóbolo", "c) La Esfinge de Giza", "d) Moái"
+                    ]
+                }
+
+            ]
+        },
+        {
+            "categoria": "Musica",
+            "preguntas": [
+                {
+                    "pregunta": "¿Quien es conocido como 'REY DEL POP'?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Elvis Presley", "b) Michael Jackson", "c) Prince", "d) Bruno Mars"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cual es el genero musical generado en Jamaica?",
+                    "respuesta": "a",
+                    "opciones": [
+                        "a) Reggae", "b) Blues", "c) Salsa", "d) Jazz"
+                    ]
+                },
+                {
+                    "pregunta": "¿Quien compuso la Novena Sinfonía?",
+                    "respuesta": "c",
+                    "opciones": [
+                        "a) Mozart", "b) Bach", "c) Beethoven", "d) Chopin"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cual de estos instrumentos pertenece a la familia de cuerdas",
+                    "respuesta": "c",
+                    "opciones": [
+                        "a) Flauta", "b) Piano", "c) Violin", "d) Trompeta"
+                    ]
+                }
+
+            ]
+        },
+        {
+            "categoria": "Geografía",
+            "preguntas": [
+                {
+                    "pregunta": "Cual es el rio mas largo del mundo?",
+                    "respuesta": "a",
+                    "opciones": [
+                        "a) Amazonas", "b) Nilo", "c) África", "d) Oceania"
+                    ]
+                },
+                {
+                    "pregunta": "¿En que continente queda Mongolia?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Europa", "b) Asia", "c) África", "d) Oceanía"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cual es la capital de Australia?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Sidney", "b) Canberra", "c) Melbourne", "d) Perth"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cual de estos paises NO tiene salida al mar?",
+                    "respuesta": "a",
+                    "opciones": [
+                        "a) Bolivia", "b) Venezuela", "c) Paraguay", "d) Mongolia"
+                    ]
+                }
+
+            ]
+        },
+        {
+            "categoria": "Deportes",
+            "preguntas": [
+                {
+                    "pregunta": "¿Que selección ha ganado mas Copas del Mundo de fútbol?",
+                    "respuesta": "d",
+                    "opciones": [
+                        "a) Alemania", "b) Italia", "c) Argentina", "d) Brasil"
+                    ]
+                },
+                {
+                    "pregunta": "¿Cuantos jugadores hay en cancha por equipo en baloncesto?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) 4", "b) 5", "c) 6", "d) 7"
+                    ]
+                },
+                {
+                    "pregunta": "¿Quién es el maximo goleador histórico del futbol?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Pelé", "b) Cristano Ronaldo", "c) Lionel Messi", "d) Ali Daei"
+                    ]
+                },
+                {
+                    "pregunta": "¿En qué país originaron los juegos Olímpicos antiguos?",
+                    "respuesta": "c",
+                    "opciones": [
+                        "a) Italia", "b) Egipto", "c) Grecia", "d) Turquía"
+                    ]
+                }
+
+            ]
+        },
+        {
+            "categoria": "Historia",
+            "preguntas": [
+                {
+                    "pregunta": "¿Quién fue el primer empeador de Roma?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) Julio César", "b) Augusto", "c) Nerón", "d) Trajano"
+                    ]
+                },
+                {
+                    "pregunta": "¿En que país comenzó la Revolucion Industrial?",
+                    "respuesta": "d",
+                    "opciones": [
+                        "a) Francia", "b) Alemania", "c) Estados Unidos", "d) Reino Unido"
+                    ]
+                },
+                {
+                    "pregunta": "Cual fue el detonante de la Primera Guerra Mundial?",
+                    "respuesta": "b",
+                    "opciones": [
+                        "a) La invasion a Polonia", "b) El asesinato del archiduque Francisco Fernando", "c) El hundimiento del Lusitania", "d) La crisis de los Balcanes"
+                    ]
+                },
+                {
+                    "pregunta": "¿Quien fue el fundador del Imperio Mongol?",
+                    "respuesta": "c",
+                    "opciones": [
+                        "a) Kublai Kan", "b) Atila", "c) Gengis Kan", "d) Tamerlán"
+                    ]
+                }
+
+            ]
+        },
+    ]
