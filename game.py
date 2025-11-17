3fb548f8ad312f0a786bc9de99e8087279f1955e
Choklitos21 <diego.mm09@hotmail.com>
2025-11-16 21:25:12 -0500

    Added users.py and users.json to save our users, also added scores.py and scores.json to save scores, added functionality to save those scores into a json file

--- game.py
+++ game.py
@@ -1,19 +1,19 @@
 import json
-
+from scores import *
 import random, time
 import os
 import platform
 
-with open("questions.json", "r", encoding='utf-8') as j:
-    questions = json.load(j)
+with open("questions.json", "r", encoding='utf-8') as json_file:
+    questions = json.load(json_file)
 
 user = {
-    "nombre": "Choklitos",
-    "puntaje": {
-        "tiempo": 0.0,
-        "vidas": 0,
-        "correctas": 0,
-        "incorrectas": 0
+    "name": "Choklitos",
+    "score": {
+        "time": 0.0,
+        "lives": 0,
+        "correct": 0,
+        "incorrect": 0
     }
 }
 
@@ -29,40 +29,47 @@ def jugarTrivia():
 
     gameRunning = True
     while gameRunning:
-        
         for i in range(1,3):
-            currentQuestion = questions[int(count)]["preguntas"][random.randint(0, 3)]
+            flag = True
+            randomOption = random.randint(0, 3)
+            while flag:
+                currentQuestion = questions[int(count)]["preguntas"][randomOption]
 
-            #Borrar luego de hacer pruebas
-            print("here",currentQuestion)
+                #Borrar luego de hacer pruebas
+                print("here",currentQuestion)
 
-            print(f"""
-            CATEGORIA: {questions[int(count)]["categoria"]}
-            Current lives: {lives}{" *Careful, it's your last life*" if lives == 1 else ""}
-            This is your question
-            {currentQuestion["pregunta"]}
-            
-            Your options are:
-            A. {currentQuestion["opciones"][0]}           B. {currentQuestion["opciones"][1]}
-            C. {currentQuestion["opciones"][2]}           D. {currentQuestion["opciones"][3]} 
-            """)
-            answer = str(input("\nWhat's your answer?: ")).strip().lower()
+                print(f"""
+                CATEGORIA: {questions[int(count)]["categoria"]}
+                Current lives: {lives}{" *Careful, it's your last life*" if lives == 1 else ""}
+                This is your question
+                {currentQuestion["pregunta"]}
+                
+                Your options are:
+                A. {currentQuestion["opciones"][0]}           B. {currentQuestion["opciones"][1]}
+                C. {currentQuestion["opciones"][2]}           D. {currentQuestion["opciones"][3]} 
+                """)
+                answer = str(input("\nWhat's your answer?: ")).strip().lower()
 
-            if not answer.isalpha():
-                print("Option not valid, try again")
-                continue
-            elif answer == currentQuestion["respuesta"] and count < 3:
-                print("!CORRECT¡\nNICE DONE\nCan you keep it up?")
-                correctAnswers += 1
-            elif answer != currentQuestion["respuesta"] and count < 3:
-                print("INCORRECT\ntry better next time ;)")
-                incorrectAnswers += 1
-                lives -= 1
+                if not answer.isalpha():
+                    print("Option not valid, try again")
+                elif answer != "a" and answer != "b" and answer != "c" and answer != "d":
+                    print("Option not valid, try with A, B, C or D")
+                elif answer == currentQuestion["respuesta"]:
+                    print("!CORRECT¡\nNICE DONE\nCan you keep it up?")
+                    correctAnswers += 1
+                    flag = False
+                elif answer != currentQuestion["respuesta"]:
+                    print("INCORRECT\ntry better next time ;)")
+                    incorrectAnswers += 1
+                    lives -= 1
+                    flag = False
 
-            time.sleep(1.5)
-            os.system('cls' if os.name == 'nt' else 'clear')
+                if lives == 0:
+                    break
 
-        if lives <= 0:
+                time.sleep(1.5)
+
+        if lives == 0:
             print("You dont have more lives, good luck next time")
             gameRunning = False
         count += 1
@@ -71,10 +78,12 @@ def jugarTrivia():
             gameRunning = False
 
     totalTime = startTime - time.time()
-    activeUser["puntaje"]["tiempo"] = totalTime
-    activeUser["puntaje"]["vidas"] = lives
-    activeUser["puntaje"]["correctas"] = correctAnswers
-    activeUser["puntaje"]["incorrectas"] = incorrectAnswers
+    activeUser["score"]["time"] = totalTime
+    activeUser["score"]["lives"] = lives
+    activeUser["score"]["correct"] = correctAnswers
+    activeUser["score"]["incorrect"] = incorrectAnswers
+
+    saveScoreMenu(activeUser)
 
     return activeUser
 
---
+++ scores.py
@@ -0,0 +1,43 @@
+import os
+import json
+
+scoreTest = {
+    "score": {
+        "time": 0.0,
+        "lives": 0,
+        "correct": 0,
+        "incorrect": 0
+    }
+}
+
+def saveScore(score):
+    if os.path.getsize("scores.json") == 0:
+        data = []  # Asigna un diccionario vacío
+    else:
+        with open("scores.json", 'r') as old_json:
+            data = json.load(old_json)
+
+    #Remove after testing
+    print(data)
+    data.append(score)
+    print(data)
+
+    with open("scores.json", "w", encoding="utf-8") as new_json:
+        json.dump(data, new_json, indent=4)
+
+#Remove after testing
+#saveScore()
+
+def saveScoreMenu(score):
+    flag = True
+    while flag:
+        option = str(input("Would you like to save your score? y/n: ")).strip().lower()
+
+        if option == "y" or option == "yes":
+            saveScore(score)
+            print("SCORE SAVED")
+            flag = False
+        elif option == "n" or option == "no":
+            flag = False
+        else:
+            print("Option not valid, try again")
