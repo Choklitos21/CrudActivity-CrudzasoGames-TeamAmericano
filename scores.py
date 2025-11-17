d393272de972c5690c8e0f82f5ab8398a3a30ec8
Choklitos21 <diego.mm09@hotmail.com>
2025-11-17 18:17:04 -0500

    remove some comments and change lenguage of questions to English

--- scores.py
+++ scores.py
@@ -1,15 +1,6 @@
 import os
 import json
 
-scoreTest = {
-    "score": {
-        "time": 0.0,
-        "lives": 0,
-        "correct": 0,
-        "incorrect": 0
-    }
-}
-
 def saveScore(score):
     if os.path.getsize("scores.json") == 0:
         data = []
@@ -17,16 +8,8 @@ def saveScore(score):
         with open("scores.json", 'r') as old_json:
             data = json.load(old_json)
 
-    #Remove after testing
-    print(data)
-    data.append(score)
-    print(data)
-
     with open("scores.json", "w", encoding="utf-8") as new_json:
         json.dump(data, new_json, indent=4)
-
-#Remove after testing
-#saveScore()
 
 def saveScoreMenu(score):
     flag = True
